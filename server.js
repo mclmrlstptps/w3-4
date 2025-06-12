708ce46eb2d4ec826e0537353c3715d8d7a1e6fd
Micaela Tapia <mclmrls29@gmail.com>
2025-06-12 17:40:29 -0400

    edits

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
 }));
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
-}));
 
 app.use(passport.initialize());
 app.use(passport.session());
